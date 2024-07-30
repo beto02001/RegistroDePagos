e47df450e72af5a5781cc235a801fa1a6dcca8af
Luis Humberto <beto020017@gmail.com>
2024-07-29 20:14:46 -0600

    Initial Commit

---
+++ RegistroDePagos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistroDePagos
+//
+//  Created by Luis Humberto on 29/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
